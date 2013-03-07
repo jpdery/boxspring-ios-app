d26c3a0f7a2a8d104dc5f6fdcaa60faddba468da
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2013-03-07 18:25:09 -0500

    Initial Commit

---
+++ BoxSpringApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BoxSpringApp
+//
+//  Created by Jean-Philippe Déry on 2013-03-07.
+//  Copyright (c) 2013 Jean-Philippe Déry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BoxSpringApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BoxSpringApp
+//
+//  Created by Jean-Philippe Déry on 2013-03-07.
+//  Copyright (c) 2013 Jean-Philippe Déry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
